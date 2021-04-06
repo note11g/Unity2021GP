95b9e32b69e4de38c4e5def06802fc905b12f675
note11 <67783062+note11g@users.noreply.github.com>
2021-04-06 23:51:10 +0900

    0406

---
+++ Platformer Game/Assets/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Animator anim;
+    float dieTimer = 0f;
+    bool isDie = false;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDie)
+        {
+            dieTimer += Time.deltaTime;
+            if (dieTimer >= 1f)
+            {
+                SoundManager.instance.PlayHitSound();
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void Die()
+    {
+        isDie = true;
+        anim.SetBool("isDie", true);
+    }
+}
